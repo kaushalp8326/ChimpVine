1528bbbc21244fd298f1a9644c3bac28d99315cb
Kaushal Patel <kaushalp8326@gmail.com>
2021-02-23 09:31:30 -0500

    Course page

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  // ion-segment
+  coursesOrGames: string;
 
   constructor() {}
+
+  // Decides which ion-segment is selected by default
+  ngOnInit() {
+    this.coursesOrGames = 'courses';
+  }
+
+  // When selection of ion-segment changes
+  segmentChanged(event: any){
+    console.log('Segment changed', event);
+  }
+
+  mathCardClick(){
+    console.log('Math clicked');
+  }
+
+  englishCardClick(){
+    console.log('English clicked');
+  }
+
+  scienceCardClick(){
+    console.log('Science clicked');
+  }
+
+  socialStudiesCardClick(){
+    console.log('Social Studies clicked');
+  }
+
+  computerCardClick(){
+    console.log('Computer clicked');
+  }
+
+  financeCardClick(){
+    console.log('Finance clicked');
+  }
+
+  spanishCardClick(){
+    console.log('Spanish clicked');
+  }
 
 }
